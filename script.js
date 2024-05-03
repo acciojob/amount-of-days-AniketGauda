f4b3860da34429e3f8f89904224e3e5092d92c0c
Aniket Gauda <122367767+AniketGauda@users.noreply.github.com>
2024-05-03 06:35:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+function daysOfAYear(year){
+    if(isLeapYear(year))
+        return 366;
+    else
+        return 365;
+}
+
+function isLeapYear(y){
+    if(y%4==0){
+        if(y%100==0){
+            if(y%400==0)
+                return 1;
+            else
+                return 0;
+        }
+        else
+            return 1;
+    }
+    else
+        return 0;
+}
